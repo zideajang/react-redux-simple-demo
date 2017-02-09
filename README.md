a20191fffb5d4b745bd95a7c09e0bada261175df
zideajang <zidea2015@163.com>
2017-02-09 10:53:25 +0800

    第三课 Action

--- README.md
+++ README.md
@@ -7,5 +7,12 @@
 1.2 课程基本要求：了解 Web 应用开发，熟悉 js/html/css
     熟悉 nodejs 和 npm。掌握一定 react 基础。会使用 webpack 及其插件
     管理 web 前端项目
-    
+
+2. 第二课 搭建环境
+2.0使用 npm 和 webpack 搭建环境
+
+3. 第三课 Action
+    3.1 创建 actions 文件
+    3.2 定义 Action type 的常量
+    3.3 创建 Action 生成器函数
 
---
+++ src/js/actions/actions.js
@@ -0,0 +1,29 @@
+//action
+// {
+//   type: ADD_TODO,
+//   text: 'Build my first Redux app'
+//}
+
+// action 类型
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+// 定义常量
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+//创建 action 生成器
+export function addTodo(text) {
+  return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(index) {
+  return { type: TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter }
+}
