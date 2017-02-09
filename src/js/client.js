19879c4c70cbe1af5131153b3b9d22acebbd5866
zideajang <zidea2015@163.com>
2017-02-09 12:18:29 +0800

    一个简单小示例说明流程

--- src/js/client.js
+++ src/js/client.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import todoApp from './reducers/'
+// import todoApp from './reducers/'
 import App from './components/App'
 
 /*let store = createStore(todoApp);
@@ -13,3 +13,25 @@ render(
   </Provider>,
   document.getElementById('root')
 )*/
+
+//创建
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([ action.text ])
+    default:
+      return state
+  }
+}
+
+let store = createStore(todos, [ 'Use Redux' ])
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Read the docs'
+})
+
+console.log(store.getState())
+render(
+  <App/>,document.getElementById('root')
+)
